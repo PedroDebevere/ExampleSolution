835ab43a201e84f85de114b5552d6fb4a0ba14c6
Pedro Debevere <pedro.debevere@skyline.be>
2022-06-29 14:45:37 +0200

    Initial commit

---
+++ ClassLibrary12Tests/Class1Tests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary12;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary12.Tests
+{
+	[TestClass()]
+	public class Class1Tests
+	{
+		[TestMethod()]
+		public void TestTest()
+		{
+			Assert.Fail();
+		}
+	}
+}
